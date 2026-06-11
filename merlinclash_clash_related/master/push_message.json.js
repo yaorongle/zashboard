c5a8248cd33fa9f96160976a68bc9169e4a420e2
Grand Master Cat <yaorongle@gmail.com>
2026-06-11 11:19:21 +0800

    Update push_message.json.js

--- merlinclash_clash_related/master/push_message.json.js
+++ merlinclash_clash_related/master/push_message.json.js
@@ -3,5 +3,5 @@
 "content2":"<em style='color: gold;'>🐯大神猫🐯魔改特供版🐯Tiger Clash：<a href='https://github.com/yaorongle' target='_blank'>Github官方维护页</a></em>",
 "content3":"<em style='color: gold;'>🎬银河录像厅：<a href='https://nf.video/hNFjm' target='_blank'>ChatGPT Plus、Netflix、Disney+、Spotify、YouTube等会员合租车，95折优惠代码：5203344</a></em>",
 "content4":"<em>🚀合作起飞：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 全专线高速机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
-"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://778007.xyz/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>五一限时优惠码：&nbsp;&nbsp;<em style='color: white;'>mbs2xkh9</em></marquee>"
+"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://778007.xyz/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【大神猫🐱互联】</a>广告位招租：&nbsp;&nbsp;<em style='color: white;'>concodigital</em></marquee>"
 }
