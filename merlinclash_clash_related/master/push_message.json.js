539bae98573063904e2351aacdb474a8992ba655
Grand Master Cat <yaorongle@gmail.com>
2026-06-11 11:08:17 +0800

    Update push_message.json.js

--- merlinclash_clash_related/master/push_message.json.js
+++ merlinclash_clash_related/master/push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>📢公告区1：📄帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
-"content2":"<em style='color: gold;'>🐯大神猫🐯魔改特供版🐯Tiger Clash：<a href='https://github.com/yaorongle' target='_blank'>Netflix、Spotify、Youtube等上车了喂，89折优惠代码：nf325【通用】</a></em>",
+"content2":"<em style='color: gold;'>🐯大神猫🐯魔改特供版🐯Tiger Clash：<a href='https://github.com/yaorongle' target='_blank'>Github官方维护页</a></em>",
 "content3":"<em style='color: gold;'>🎬银河录像厅：<a href='https://nf.video/hNFjm' target='_blank'>ChatGPT Plus、Netflix、Disney+、Spotify、YouTube等会员合租车，95折优惠代码：5203344</a></em>",
 "content4":"<em>🚀合作起飞：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 全专线高速机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
 "content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://778007.xyz/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>五一限时优惠码：&nbsp;&nbsp;<em style='color: white;'>mbs2xkh9</em></marquee>"
