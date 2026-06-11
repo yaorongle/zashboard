42cee06feebddbf83f1079b31903050d99736426
Grand Master Cat <yaorongle@gmail.com>
2026-06-11 11:26:00 +0800

    Update push_message.json.js

--- merlinclash_clash_related/master/push_message.json.js
+++ merlinclash_clash_related/master/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "content1":"<em style='color: gold;'>📢公告区1：📄帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content2":"<em style='color: gold;'>🐯大神猫🐯魔改特供版🐯Tiger Clash：<a href='https://github.com/yaorongle' target='_blank'>Github官方维护页</a></em>",
-"content3":"<em style='color: gold;'>🎬银河录像厅：<a href='https://nf.video/hNFjm' target='_blank'>ChatGPT Plus、Netflix、Disney+、Spotify、YouTube等会员合租车，95折优惠代码：5203344</a></em>",
+"content3":"<em style='color: gold;'>🦞Cici大王的AI小铺：<a href='https://aicc888.org/' target='_blank'>ChatGPT Plus、Gemini、Claude Pro/Max、Super Grok会员充值</a></em>",
 "content4":"<em>🚀合作起飞：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 全专线高速机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
 "content5":"<marquee><em style='color: gold;'>技术支持中心：</em>&nbsp;&nbsp;<a href='https://github.com/yaorongle' target='_blank'>【大神猫🐱互联】</a>广告位招租：&nbsp;&nbsp;<em style='color: white;'>concodigital</em></marquee>"
 }
