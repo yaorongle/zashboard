a9058e8709ed123de78ede596f32f9c2ab5e61bd
Grand Master Tiger <yaorongle@gmail.com>
2026-06-18 16:42:42 +0800

    Update push_message.json.js

--- merlinclash_clash_related/master/push_message.json.js
+++ merlinclash_clash_related/master/push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>📢公告区1：📄帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
-"content2":"<em style='color: gold;'>🧚精灵猫🧚魔改特供版🧚Fairy Clash：<a href='https://github.com/yaorongle' target='_blank'>Github官方维护页</a></em>",
+"content2":"<em style='color: gold;'>🧚Fairy Clash🧚精灵猫🧚魔改特供版：<a href='https://github.com/yaorongle' target='_blank'>Github官方维护页</a></em>",
 "content3":"<em style='color: gold;'>🦞Cici大王的AI小铺：<a href='https://aicc888.org/' target='_blank'>ChatGPT Plus、Gemini、Claude Pro/Max、Super Grok会员充值</a></em>",
 "content4":"<em>🚀合作起飞：</em><em style='color: gold;'>yaorongle</em><a href='https://github.com/yaorongle' target='_blank'> 🧚| 专线高速机场 / 流媒体解锁 / Tiktok / ChatGPT |🧚‍♀️<em>赶快联系客服</em></a>",
 "content5":"<marquee><em style='color: gold;'>技术支持中心：</em>&nbsp;&nbsp;<a href='https://github.com/yaorongle' target='_blank'>【精灵猫🧚互联】</a>广告位招租：&nbsp;&nbsp;<em style='color: white;'>yaorongle</em></marquee>"
